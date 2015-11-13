298c4ab67a1e9b8b111b80c5c65b7e0599663c23
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2015-11-13 10:39:23 +0100

    Better show the drawbacks of using a round-robin pool of workers

--- examples/pool_of_workers_round_robin/functions.py
+++ examples/pool_of_workers_round_robin/functions.py
@@ -2,14 +2,23 @@ import time
 
 
 def loop(agent):
-    for i in range(13):
-        agent.send('push', i)
+    for i in range(10):
+        if not i % 5:
+            agent.send('push', 5)
+        else:
+            agent.send('push', 1)
 
 
 def print_received(self, message):
-    print(message)
+    try:
+        t0 = self.t0
+    except AttributeError as error:
+        self.t0 = time.time()
+        return
+    t1 = time.time()
+    print('[%02.0f since first] %s' % (t1 - t0, message))
 
 
 def heavy_processing(self, message):
-    time.sleep(2)
-    self.send('results', message * 2)
+    time.sleep(message)
+    self.send('results', message)

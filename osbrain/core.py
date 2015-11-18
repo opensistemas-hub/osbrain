72a9262c1c6635c595e210f36181ea1804933434
Miguel Sánchez de León Peque <msanchez@opensistemas.com>
2015-11-18 18:17:45 +0100

    Add TODO tag

--- osbrain/core.py
+++ osbrain/core.py
@@ -22,6 +22,7 @@ Pyro4.config.SERIALIZERS_ACCEPTED.add('pickle')
 Pyro4.config.SERIALIZER = 'pickle'
 Pyro4.config.THREADPOOL_SIZE = 16
 Pyro4.config.SERVERTYPE = 'multiplex'
+# TODO: should we set COMMTIMEOUT as well?
 
 
 # TODO:

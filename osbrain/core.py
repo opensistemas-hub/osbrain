65d28db41ae14ded5f21a1ce2b91d5f27336b649
Miguel Sánchez de León Peque <msanchez@opensistemas.com>
2015-11-25 17:50:00 +0100

    Implement basic FTP-related features in the BaseAgent class

--- osbrain/core.py
+++ osbrain/core.py
@@ -394,6 +394,42 @@ class BaseAgent():
     def recv(self, address):
         return self.socket[address].recv_pyobj()
 
+    def ftp_configure(self, addr, user, passwd, path, perm='elr'):
+        from pyftpdlib.authorizers import DummyAuthorizer
+        from pyftpdlib.handlers import FTPHandler
+        from pyftpdlib.servers import FTPServer
+        # Create authorizer
+        authorizer = DummyAuthorizer()
+        authorizer.add_user(user, passwd, path, perm=perm)
+        # Create handler
+        handler = FTPHandler
+        handler.authorizer = authorizer
+        # Create server
+        host, port = address_to_host_port(addr)
+        # TODO: is this necessary? Or would `None` be sufficient?
+        if port is None:
+            port = 0
+        self.ftp_server = FTPServer((host, port), handler)
+        return self.ftp_server.socket.getsockname()
+
+    @Pyro4.oneway
+    def ftp_run(self):
+        # Serve forever
+        self.ftp_server.serve_forever()
+
+    def ftp_addr(self):
+        return self.ftp_server.socket.getsockname()
+
+    def ftp_retrieve(self, addr, origin, destiny, user, passwd):
+        import ftplib
+        host, port = addr
+        ftp = ftplib.FTP()
+        ftp.connect(host, port)
+        ftp.login(user, passwd)
+        ftp.retrbinary('RETR %s' % origin, open(destiny, 'wb').write)
+        ftp.close()
+        return destiny
+
     @Pyro4.oneway
     def run(self):
         """

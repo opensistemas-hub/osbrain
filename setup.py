f11d0f9090860b05d2393fa87c0a6e32baeacbc7
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2016-07-06 16:06:31 +0200

    Initial tox testing integration

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ from os import path
 from setuptools import setup
 
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
-    long_description = f.read()
-
 setup(
     name='osbrain',
     version='0.1.0',
     description='A general-purpose multi-agent-system module',
-    long_description=long_description,
-    url='', # TODO
+    long_description='''A general-purpose multi-agent-system module written
+        in Python. It uses ZeroMQ for flexible and efficient communications
+        between agents and Pyro4 to ease configuration and deployment.''',
+    url='',  # TODO
     author='Miguel Sánchez de León Peque',
     author_email='msdeleon@opensistemas.com',
     license='AGPL',
@@ -22,7 +21,8 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'License :: OSI Approved :: GNU Affero General Public License v3 ' + \
+            'or later (AGPLv3+)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',

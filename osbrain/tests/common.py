dd65fb18ceac6ad7fd356943010401e0808aae05
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2016-09-19 16:58:39 +0200

    Compatibility changes for `pytest` 3

--- osbrain/tests/common.py
+++ osbrain/tests/common.py
@@ -2,14 +2,14 @@ import pytest
 from osbrain import run_nameserver
 
 
-@pytest.yield_fixture(scope='function')
+@pytest.fixture(scope='function')
 def nsaddr(request):
     ns = run_nameserver()
     yield ns.addr()
     ns.shutdown()
 
 
-@pytest.yield_fixture(scope='function')
+@pytest.fixture(scope='function')
 def nsproxy(request):
     ns = run_nameserver()
     yield ns

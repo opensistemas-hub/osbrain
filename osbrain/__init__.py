7473a70893a0f31ba717e26a3f508d0adc5026f9
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2016-09-19 19:04:16 +0200

    Make osBrain compatible with latest Pyro4 changes

--- osbrain/__init__.py
+++ osbrain/__init__.py
@@ -3,6 +3,7 @@ Pyro4.config.SERIALIZERS_ACCEPTED.add('pickle')
 Pyro4.config.SERIALIZER = 'pickle'
 Pyro4.config.THREADPOOL_SIZE = 16
 Pyro4.config.SERVERTYPE = 'multiplex'
+Pyro4.config.REQUIRE_EXPOSE = False
 # TODO: should we set COMMTIMEOUT as well?
 Pyro4.config.DETAILED_TRACEBACK = True
 

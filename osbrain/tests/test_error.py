0f8479a1e4c94ec0ee32db634f0c05c9742c32bf
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2016-09-19 19:13:22 +0200

    No need to shutdown when agent/NS did not start

--- osbrain/tests/test_error.py
+++ osbrain/tests/test_error.py
@@ -11,7 +11,6 @@ def test_ns_error_os(nsaddr):
     ns = NameServerProcess(nsaddr)
     try:
         ns.start()
-        ns.shutdown()
         assert 0
     except RuntimeError:
         pass
@@ -27,7 +26,6 @@ def test_ns_error_permission():
     ns = NameServerProcess('127.0.0.1:22')
     try:
         ns.start()
-        ns.shutdown()
         assert 0
     except RuntimeError:
         pass
@@ -42,7 +40,6 @@ def test_agent_error_os(nsaddr):
     agent = Agent('a0', nsaddr, nsaddr)
     try:
         agent.start()
-        agent.shutdown()
         assert 0
     except RuntimeError:
         pass
@@ -57,7 +54,6 @@ def test_agent_error_permission(nsaddr):
     agent = Agent('a0', nsaddr, '127.0.0.1:22')
     try:
         agent.start()
-        agent.shutdown()
         assert 0
     except RuntimeError:
         pass

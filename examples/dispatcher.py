03a7d5c79b3e97407b7f26148a35c26a644f6787
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2016-10-26 18:55:59 +0200

    Refacored dispatcher and ventilator examples

--- examples/dispatcher.py
+++ examples/dispatcher.py
@@ -1,44 +1,35 @@
 import time
-from osbrain import random_nameserver
 from osbrain import run_agent
-
-
-def log(agent, message):
-    agent.log_info(message)
+from osbrain import run_nameserver
 
 
 def rep_handler(agent, message):
-    if agent.i < 10:
-        if not agent.i % 5:
-            agent.send('rep', 5)
-        else:
-            agent.send('rep', 1)
-    agent.i += 1
+    if not agent.tasks:
+        return None
+    return agent.tasks.pop()
 
 
-def worker_loop(agent):
-    while True:
-        agent.send('dispatcher', 'READY!')
-        x = agent.recv('dispatcher')
-        time.sleep(x)
-        agent.send('results', '%s finished with %s' % (agent.name, x))
+def request_work(agent):
+    x = agent.send_recv('dispatcher', 'READY!')
+    if not x:
+        agent.shutdown()
+    time.sleep(x)
+    agent.send('results', '%s finished with %s' % (agent.name, x))
 
 
 if __name__ == '__main__':
 
-    ns = random_nameserver()
+    ns = run_nameserver()
 
-    results = run_agent('Results', nsaddr=ns)
-    results_addr = results.bind('PULL', handler=log)
+    results = run_agent('Results')
+    results_addr = results.bind('PULL', handler=lambda x, y: x.log_info(y))
 
-    dispatcher = run_agent('Dispatcher', nsaddr=ns)
-    dispatcher.set_attr(i=0)
+    dispatcher = run_agent('Dispatcher')
+    dispatcher.set_attr(tasks=[1, 1, 1, 1, 5, 1, 1, 1, 1, 5])
     dispatcher_addr = dispatcher.bind('REP', alias='rep', handler=rep_handler)
 
     for i in range(5):
-        worker = run_agent('Worker%s' % i, nsaddr=ns)
+        worker = run_agent('Worker%s' % i)
         worker.connect(results_addr, alias='results')
         worker.connect(dispatcher_addr, alias='dispatcher')
-        worker.stop()
-        worker.set_loop(worker_loop)
-        worker.run()
+        worker.each(0., request_work)
--- examples/ventilator.py
+++ examples/ventilator.py
@@ -1,10 +1,6 @@
 import time
-from osbrain import random_nameserver
 from osbrain import run_agent
-
-
-def log(agent, message):
-    agent.log_info(message)
+from osbrain import run_nameserver
 
 
 def heavy_processing(agent, message):
@@ -15,22 +11,18 @@ def heavy_processing(agent, message):
 
 if __name__ == '__main__':
 
-    ns = random_nameserver()
+    run_nameserver()
 
-    results = run_agent('Results', nsaddr=ns)
-    results_addr = results.bind('PULL', handler=log)
+    results = run_agent('Results')
+    results.bind('PULL', alias='results', handler=lambda a, m: a.log_info(m))
 
-    ventilator = run_agent('Ventilator', nsaddr=ns)
+    ventilator = run_agent('Ventilator')
     ventilator_addr = ventilator.bind('PUSH', alias='push')
 
     for i in range(5):
-        worker = run_agent('Worker%s' % i, nsaddr=ns)
-        worker.connect(results_addr, alias='results')
+        worker = run_agent('Worker%s' % i)
+        worker.connect(results.addr('results'), alias='results')
         worker.connect(ventilator_addr, handler=heavy_processing)
 
-    # Send tasks
-    for i in range(10):
-        if not i % 5:
-            ventilator.send('push', 5)
-        else:
-            ventilator.send('push', 1)
+    for task in [5, 1, 1, 1, 1, 5, 1, 1, 1, 1]:
+        ventilator.send('push', task)

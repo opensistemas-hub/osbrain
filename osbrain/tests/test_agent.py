1336344bb1b5f0a2a24712a2fa6679f97344b2ab
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2016-12-20 13:41:49 +0100

    Add agent bind to specified address test

--- osbrain/tests/test_agent.py
+++ osbrain/tests/test_agent.py
@@ -3,6 +3,7 @@ Test file for agents.
 """
 import os
 import time
+import random
 from uuid import uuid4
 from threading import Timer
 
@@ -14,6 +15,7 @@ from osbrain import Agent
 from osbrain import AgentProcess
 from osbrain import Proxy
 from osbrain import NSProxy
+from osbrain import SocketAddress
 
 from common import nsaddr  # pragma: no flakes
 from common import nsproxy  # pragma: no flakes
@@ -529,3 +531,29 @@ def test_agent_bind_transport(nsaddr):
     os.environ['OSBRAIN_DEFAULT_TRANSPORT'] = 'ipc'
     address = agent.bind('PUSH', transport='ipc')
     assert address.transport == 'ipc'
+
+
+def test_agent_bind_given_address(nsaddr):
+    """
+    Test agent binding to an specified address using TCP and IPC transport
+    layers.
+    """
+    agent = run_agent('a0')
+    # IPC
+    ipc_addr = str(uuid4())
+    address = agent.bind('PUSH', addr=ipc_addr, transport='ipc')
+    assert address.transport == 'ipc'
+    assert address.address == ipc_addr
+    # TCP
+    while True:
+        try:
+            # Bind to random port
+            port = random.randrange(10000, 20000)
+            tcp_addr = '127.0.0.1:%s' % port
+            tcp_addr = SocketAddress('127.0.0.1', port)
+            address = agent.bind('PUSH', addr=tcp_addr, transport='tcp')
+            break
+        except Exception:
+            pass
+    assert address.transport == 'tcp'
+    assert address.address == tcp_addr

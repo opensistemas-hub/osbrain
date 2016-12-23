6aa7d2c35978a4f4fe1045aaae69ef3f521c256c
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2016-12-23 08:45:33 +0100

    Update Pyro4 dependency
    A version supporting dill serializer is required.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     ],
     keywords='osbrain multi-agent system',
     packages=['osbrain'],
-    install_requires=['Pyro4>=4.40', 'pyzmq>=15.2.0', 'dill>=0.2.0'],
+    install_requires=['Pyro4>=4.42', 'pyzmq>=15.2.0', 'dill>=0.2.0'],
     extras_require={
         'dev': [],
         'test': ['tox'],

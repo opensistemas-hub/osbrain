4486fc5952a9224b96cc2a9df6d456f47aa925d8
Guillermo Alonso <galonso@opensistemas.com>
2017-02-15 12:40:43 +0100

    Add tests for the behaviour of raw PUBSUB communication.

--- osbrain/tests/test_agent_pubsub_topics.py
+++ osbrain/tests/test_agent_pubsub_topics.py
@@ -10,7 +10,7 @@ def log_received_to_list(agent, message, topic=None):
     agent.received_list.append(message)
 
 
-def test_pubsub(nsaddr):
+def test_pubsub_topics_pickle(nsaddr):
     """
     Simple publisher-subscriber pattern test.
 
@@ -27,7 +27,7 @@ def test_pubsub(nsaddr):
     for agent in (a1, a2, a3, a4, a5):
         agent.set_attr(received_list=[])
 
-    addr = a0.bind('PUB', alias='pub')
+    addr = a0.bind('PUB', alias='pub', serializer='pickle')
 
     a1.connect(addr, handler=log_received_to_list)
     a2.connect(addr, handler={'foo': log_received_to_list})
@@ -80,3 +80,79 @@ def test_pubsub(nsaddr):
     assert message_02 in a5.get_attr('received_list')
     assert message_03 in a5.get_attr('received_list')
     assert message_04 in a5.get_attr('received_list')
+
+
+def test_pubsub_topics_raw(nsaddr):
+    """
+    Simple publisher-subscriber pattern test.
+
+    Different messages sent with different agents subscribed to different
+    topics are tested within this method.
+
+    In the raw version of PUBSUB, we want to replicate the raw message passing
+    of ZMQ, in which the topic is passed along the message and it is the
+    responsability of the handler to split them.
+    """
+    a0 = run_agent('a0')
+    a1 = run_agent('a1')
+    a2 = run_agent('a2')
+    a3 = run_agent('a3')
+    a4 = run_agent('a4')
+    a5 = run_agent('a5')
+
+    for agent in (a1, a2, a3, a4, a5):
+        agent.set_attr(received_list=[])
+
+    addr = a0.bind('PUB', alias='pub', serializer='raw')
+
+    a1.connect(addr, handler=log_received_to_list)
+    a2.connect(addr, handler={'foo': log_received_to_list})
+    a3.connect(addr, handler={'bar': log_received_to_list,
+                              'foo': log_received_to_list})
+    a4.connect(addr, handler={'bar': log_received_to_list})
+    a5.connect(addr, handler={'fo': log_received_to_list})
+
+    # Give some time for all the agents to connect
+    time.sleep(0.1)
+
+    # Send some messages
+    message_01 = b'Hello'
+    a0.send('pub', message_01)
+
+    message_02 = b'World'
+    a0.send('pub', message_02, topic='foo')
+
+    message_03 = b'FOO'
+    a0.send('pub', message_03, topic='foobar')
+
+    message_04 = b'BAR'
+    a0.send('pub', message_04, topic='fo')
+
+    # Give some time for all the agents to handle the message
+    time.sleep(0.1)
+
+    # Check each agent received the corresponding messages
+    assert message_01 in a1.get_attr('received_list')
+    assert b'fooWorld' in a1.get_attr('received_list')
+    assert b'foobarFOO' in a1.get_attr('received_list')
+    assert b'foBAR' in a1.get_attr('received_list')
+
+    assert message_01 not in a2.get_attr('received_list')
+    assert b'fooWorld' in a2.get_attr('received_list')
+    assert b'foobarFOO' in a2.get_attr('received_list')
+    assert b'foBAR' not in a2.get_attr('received_list')
+
+    assert message_01 not in a3.get_attr('received_list')
+    assert b'fooWorld' in a3.get_attr('received_list')
+    assert b'foobarFOO' in a3.get_attr('received_list')
+    assert b'foBAR' not in a3.get_attr('received_list')
+
+    assert message_01 not in a4.get_attr('received_list')
+    assert b'fooWorld' not in a4.get_attr('received_list')
+    assert b'foobarFOO' not in a4.get_attr('received_list')
+    assert b'foBAR' not in a4.get_attr('received_list')
+
+    assert message_01 not in a5.get_attr('received_list')
+    assert b'fooWorld' in a5.get_attr('received_list')
+    assert b'foobarFOO' in a5.get_attr('received_list')
+    assert b'foBAR' in a5.get_attr('received_list')

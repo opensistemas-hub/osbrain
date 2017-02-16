d7383d4bdcaf12344ca5202d600a6c19ac89482e
Guillermo Alonso <guialonun@gmail.com>
2017-02-16 23:50:27 +0100

    Example files turned into real python code

--- examples/explicit_serializer_agent.py
+++ examples/explicit_serializer_agent.py
@@ -1,6 +1,9 @@
-...
+from osbrain import run_agent
 
-a1 = run_agent('a1', serializer='json')
-addr1 = a1.bind('PUB', 'alias1')
 
-...
+if __name__ == '__main__':
+    a1 = run_agent('a1', serializer='json')
+    addr1 = a1.bind('PUB', 'alias1')
+
+    _address = a1.get_attr('address')
+    assert _address[addr1].serializer == 'json'
--- examples/explicit_serializer_socket.py
+++ examples/explicit_serializer_socket.py
@@ -1,6 +1,9 @@
-...
+from osbrain import run_agent
 
-a1 = run_agent('a1')
-addr1 = a1.bind('PULL', handler=set_received, serializer='pickle')
 
-...
+if __name__ == '__main__':
+    a1 = run_agent('a1')
+    addr1 = a1.bind('PUB', 'alias1', serializer='raw')
+
+    _address = a1.get_attr('address')
+    assert _address[addr1].serializer == 'raw'

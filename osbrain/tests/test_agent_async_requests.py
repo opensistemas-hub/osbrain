ca09af3687f65693bea88020417c7cd423ea41c2
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2017-03-08 18:25:21 +0100

    Clean-up asynchronous requests test file

--- osbrain/tests/test_agent_async_requests.py
+++ osbrain/tests/test_agent_async_requests.py
@@ -8,41 +8,17 @@ from osbrain import run_agent
 from common import nsproxy  # pragma: no flakes
 
 
-# addr = server.bind('PULL', alias='replier', handler=foo)
-# client.connect(addr)
-#
-# # When binding, a PULL socket is created (to recieve all requests),
-# # requests are handled with the same handler
-# addr = server.bind('ASYNC_REP', alias='replier', handler=foo)
-# # When connecting to an ASYNC_REP some things happen:
-# #  - A PUSH socket is created with an alias
-# #  - Client UUID is sent, along with a PULL AgentAddress, so the server
-# #    knows where to send requests that come from that agent. Should a new
-# #    PULL socket be created instead for each connection?
-# client.connect(addr, alias='async')
-# #  - Send will have a special send_async_req case
-# #  - handler, wait, repeat, on_error could have default values set on connect()
-# #  - send will add UUID of the agent and an UUID for the request
-# #  - a timer is created to wait for the response before raising...
-# client.send('async', 'a request here...',
-#             handler=bar, wait=3, on_error=omg)
-# # For PUB-SUB there will be another special AddressKind (i.e.: STREAM?),
-# #  - A SUB socket is created with an alias
-# client.send('sync', 'a request here...',
-#             handler=bar, wait=3, repeat=2, on_error=omg)
-
-
 def test_return(nsproxy):
     """
     Asynchronous request-reply pattern with a reply handler that returns.
     """
-    def late_reply(agent, message):
-        agent.received.append(message)
+    def late_reply(agent, request):
+        agent.received.append(request)
         time.sleep(1)
-        return message
+        return 'x' + request
 
-    def receive(agent, message):
-        agent.received.append('x' + message)
+    def receive(agent, response):
+        agent.received.append(response)
 
     server = run_agent('server')
     client = run_agent('client')
@@ -76,14 +52,14 @@ def test_yield(nsproxy):
     """
     Asynchronous request-reply pattern with a reply handler that yields.
     """
-    def late_reply(agent, message):
-        agent.received.append(message)
+    def late_reply(agent, request):
+        agent.received.append(request)
         time.sleep(1)
-        yield message
-        agent.received.append(message)
+        yield 'x' + request
+        agent.received.append('y' + request)
 
-    def receive(agent, message):
-        agent.received.append('x' + message)
+    def receive(agent, response):
+        agent.received.append(response)
 
     server = run_agent('server')
     client = run_agent('client')
@@ -106,16 +82,16 @@ def test_yield(nsproxy):
 
     # Wait for client to receive reply asynchronously
     time.sleep(1)
-    assert server.get_attr('received') == ['foo', 'foo', 'bar']
+    assert server.get_attr('received') == ['foo', 'yfoo', 'bar']
     assert client.get_attr('received') == ['xfoo']
     time.sleep(1)
-    assert server.get_attr('received') == ['foo', 'foo', 'bar', 'bar']
+    assert server.get_attr('received') == ['foo', 'yfoo', 'bar', 'ybar']
     assert client.get_attr('received') == ['xfoo', 'xbar']
 
 
 def test_wait(nsproxy):
     # TODO
-    #client.send('async', 'Hello world!', wait=3)
+    # client.send('async', 'Hello world!', wait=3)
     pass
 
 
@@ -123,5 +99,5 @@ def test_wait_on_error(nsproxy):
     def on_error(agent):
         pass
     # TODO
-    #client.send('async', 'Hello world!', wait=3, on_error=on_error)
+    # client.send('async', 'Hello world!', wait=3, on_error=on_error)
     pass

719ae316ce8b2e6130303274d4d46454d52e35d4
Guillermo Alonso <galonso@opensistemas.com>
2017-03-17 14:06:51 +0100

    Improve examples

--- examples/explicit_serializer_global.py
+++ examples/explicit_serializer_global.py
@@ -1,9 +1,11 @@
 '''
-Simple example that will demonstrate how to set the serializer at per agent
-level.
+Simple example that will demonstrate how to set the serializer at global level
+through an environment variable.
 
 Note that a crash is intended, since json does not support byte serialization.
 '''
+import os
+
 from osbrain import run_nameserver
 from osbrain import run_agent
 
@@ -13,10 +15,12 @@ def set_received(agent, message, topic=None):
 
 
 if __name__ == '__main__':
+    os.environ['OSBRAIN_DEFAULT_SERIALIZER'] = 'json'
+
     ns = run_nameserver()
 
     a0 = run_agent('a0')
-    a1 = run_agent('a1', serializer='json')
+    a1 = run_agent('a1')
     addr = a1.bind('PULL', handler=set_received)
     a0.connect(addr, 'push')
     message = b'Hello world'
--- examples/explicit_serializer_socket.py
+++ examples/explicit_serializer_socket.py
@@ -1,9 +1,28 @@
+'''
+Simple example that will demonstrate how to set the serializer at per socket
+level.
+
+Note that a crash is intended, since json does not support byte serialization.
+'''
+from osbrain import run_nameserver
 from osbrain import run_agent
 
 
-if __name__ == '__main__':
-    a1 = run_agent('a1')
-    addr1 = a1.bind('PUB', 'alias1', serializer='raw')
+def set_received(agent, message, topic=None):
+    print('Got a message: {}'.format(message))
 
-    _address = a1.get_attr('address')
-    assert _address[addr1].serializer == 'raw'
+
+if __name__ == '__main__':
+    ns = run_nameserver()
+
+    a0 = run_agent('a0')
+    a1 = run_agent('a1')
+    addr = a1.bind('PULL', handler=set_received, serializer='json')
+    a0.connect(addr, 'push')
+    message = b'Hello world'
+    try:
+        a0.send('push', message)
+    except:
+        print('Something went wrong...')
+
+    ns.shutdown()

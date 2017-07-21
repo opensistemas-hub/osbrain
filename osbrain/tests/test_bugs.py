2f2ae9adf45b72f1e958af9fb7d52489bf66acc6
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2017-07-21 11:52:40 +0200

    Simplify `test_timer_recursion` test
    Improve coverage and make use of the `wait_agent_condition` function.

--- osbrain/tests/test_bugs.py
+++ osbrain/tests/test_bugs.py
@@ -2,9 +2,9 @@
 Test file for bugs found in osbrain.
 """
 import sys
-import time
 
 from osbrain import run_agent
+from osbrain.helper import wait_agent_condition
 
 from common import nsproxy  # pragma: no flakes
 
@@ -21,10 +21,7 @@ def test_timer_recursion(nsproxy):
     agent = run_agent('a0')
     agent.set_attr(count=0)
     agent.each(0.0, inc)
-    while True:
-        count = agent.get_attr('count')
-        if count > sys.getrecursionlimit():
-            return
-        time.sleep(0.1)
-        if count == agent.get_attr('count'):
-            raise RuntimeError('Probable recursion level exceeded!')
+
+    limit = sys.getrecursionlimit()
+    assert wait_agent_condition(agent, lambda agent: agent.count > limit,
+                                timeout=10)

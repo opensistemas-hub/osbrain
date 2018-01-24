a03cdd09567c6209e712488ef84e23b139934f31
Oscar Caballero <ocaballero@opensistemas.com>
2018-01-24 15:05:48 +0100

    Check that the sockets are being closed

---
+++ osbrain/tests/test_agent_ipc_sockets.py
@@ -0,0 +1,83 @@
+"""
+Test file for everything IPC socket related
+"""
+import pytest
+
+from osbrain import run_agent
+from osbrain import run_nameserver
+from osbrain.helper import agent_dies
+
+from common import nsproxy  # pragma: no flakes
+
+
+def test_agent_close_ipc_socket_agent_shutdown(nsproxy):
+    """
+    Check that the socket is closed and the socket file removed after the agent
+    is shut down.
+    """
+    agent = run_agent('name')
+    address = agent.bind('PUSH')
+    agent.shutdown()
+
+    assert agent_dies('name', nsproxy, timeout=3)
+    assert not address.address.exists()
+
+
+def test_agent_close_ipc_socket_agent_kill(nsproxy):
+    """
+    Check that the socket is closed and the socket file removed after the agent
+    is killed.
+    """
+    agent = run_agent('name')
+    address = agent.bind('PUSH')
+    agent.kill()
+
+    assert agent_dies('name', nsproxy, timeout=3)
+    assert not address.address.exists()
+
+
+def test_agent_close_ipc_socket_agent_blocked_nameserver_shutdown():
+    """
+    Check that the socket is closed and the socket file removed when the name
+    server is shut down having a blocked agent.
+    """
+    def block(agent):
+        agent.send('out', 'blocking...')
+
+    ns = run_nameserver()
+    blocker = run_agent('blocker')
+    blocker.set_method(block)
+    addr = blocker.bind('PUSH', alias='out')
+    blocker.after(0, 'block')
+    ns.shutdown(timeout=1.)
+
+    assert not addr.address.exists()
+
+
+def test_agent_close_ipc_socket_agent_crash_nameserver_shutdown():
+    """
+    Check that the socket is closed and the socket file removed after the agent
+    crashes and the name server calls for shutdown.
+    """
+    ns = run_nameserver()
+    agent = run_agent('agent')
+    addr = agent.bind('PUSH', 'main')
+    with pytest.raises(RuntimeError):
+        agent.raise_exception()
+
+    ns.shutdown()
+
+    assert not addr.address.exists()
+
+
+def test_agent_close_ipc_socket_nameserver_shutdown():
+    """
+    Check that the socket is closed and the socket file removed after the name
+    server is shut down.
+    """
+    ns = run_nameserver()
+    agent = run_agent('agent')
+    addr = agent.bind('PUSH', 'main')
+    ns.shutdown()
+
+    assert not addr.address.exists()

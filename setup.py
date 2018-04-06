969c7a1802734dfe2119aa7cf5f89818421da559
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2018-04-06 15:10:22 +0200

    Check no print statements left in source code

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
             'flake8',
             'flake8-bugbear',
             'flake8-per-file-ignores',
+            'flake8-print',
             'flake8-quotes',
             'pep8-naming',
         ],

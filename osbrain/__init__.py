cec1085b0f876cbd11cf959e383ded6524e5615e
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2018-04-09 14:34:23 +0200

    Bump version to 0.6.1

--- osbrain/__init__.py
+++ osbrain/__init__.py
@@ -39,7 +39,7 @@ else:
     config['IPC_DIR'].mkdir(exist_ok=True, parents=True)
 
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 
 from .agent import Agent, AgentProcess, run_agent
 from .nameserver import NameServer, run_nameserver

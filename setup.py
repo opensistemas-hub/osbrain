91a3bf28d59499e9658bbfccde9070e23b9fe517
Miguel Sánchez de León Peque <msdeleon@opensistemas.com>
2019-02-26 11:39:05 +0100

    Drop Python 3.4 support already (reaching EoL)

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
